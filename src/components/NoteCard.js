b50811568038c44fc1809e28d1d7e2741fbc131d
Bappaditya <721135bappa@gmail.com>
2023-01-13 12:08:24 +0530

    Add title and note

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,8 +1,8 @@
 export default function NoteCard(props) {
     return (
         <div className="note-card">
-            <p>{props.title}</p>
-            <p>{props.note}</p>
+            <p>Title- {props.title}</p>
+            <p>Note- {props.note}</p>
         </div>
     );
 }
