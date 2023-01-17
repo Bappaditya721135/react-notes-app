0893b18ad8e9a46d54cc0e94581f05f5f6ec180a
Bappaditya <721135bappa@gmail.com>
2023-01-17 16:29:34 +0530

    style: fix some style

--- src/components/BigNote.js
+++ src/components/BigNote.js
@@ -1,15 +1,12 @@
 export default function BigNote(props) {
     return(
         <div className="big-note-card">
-            <div onClick={props.handleCardClick} className="cross-btn">x</div>
-            <div className="note-date"><span>note written on -</span>{`${props.day}/${props.month}/${props.year}`}</div>
+            <div className="note-date">Date-{`${props.day}/${props.month}/${props.year}`}</div>
             <div className="title-section">
-                <p className="big-note-heading">Title-</p>
                 <p className="big-note-title">{props.title}</p>
             </div>
             <div className="note-section">
-                <p className="big-note-heading">Note-</p>
-                <textarea className="big-note-disc" defaultValue={props.note}></textarea>
+                <div className="big-note-disc">{props.note}</div>
             </div>
         </div>
     );
