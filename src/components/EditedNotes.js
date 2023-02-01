5f43716445b2abbf3e474806660b8d922f32f732
Bappaditya <721135bappa@gmail.com>
2023-02-01 13:49:04 +0530

    code-restructure: EditedNotes.js

--- src/components/EditedNotes.js
+++ src/components/EditedNotes.js
@@ -1,17 +1,20 @@
 import NoteCard from "./NoteCard";
 
 export default function EditedNotes(props) {
+    const arr = props.data.filter(obj => obj.edited === true);
     return(
         <div className="edited-notes-container">
-            {props.editedNotes.length > 0 ? props.editedNotes.map(obj => <NoteCard 
-                                    key={obj.id} 
-                                    id={obj.id} 
-                                    title={obj.title} 
-                                    note={obj.note} 
-                                    date={obj.date} 
-                                    editData={props.editData} 
-                                    edited={obj.edited} />
-            ) : <p className="default-text">You currently dont't have any edited notes</p>}
+            {arr.length > 0 ? 
+            arr.map(obj => <NoteCard 
+                            key={obj.id} 
+                            id={obj.id} 
+                            title={obj.title} 
+                            note={obj.note} 
+                            date={obj.date}
+                            edited={obj.edited}
+                            editDate={obj.editDate}
+                 />)
+            : <p className="default-text">You currently dont't have any edited notes</p>}
         </div>
     );
 }
